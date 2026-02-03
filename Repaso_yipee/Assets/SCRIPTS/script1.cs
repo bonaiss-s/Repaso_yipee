3fd192ada52d50a2d097bc377ceff6a7c074e4cd
JuanPablo827 <jtorres827@soyudemedellin.edu.co>
2026-02-03 13:27:32 -0500

    Creacion_proyecto

---
+++ Repaso_yipee/Assets/SCRIPTS/script1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxcollider: MonoBehaviour
+{
+    [SerializeField]
+
+   private BoxCollider _collider ;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
