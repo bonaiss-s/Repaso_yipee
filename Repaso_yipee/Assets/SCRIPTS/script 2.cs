6cc41fa2b62a0d1281131d01a11b649434e5b6f9
JuanPablo827 <jtorres827@soyudemedellin.edu.co>
2026-02-05 13:31:19 -0500

    practica2

---
+++ Repaso_yipee/Assets/SCRIPTS/script 2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script2 : MonoBehaviour
+{
+    public string[] yipeeee = new string["uno", "dos", "cuatro"];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Repaso_yipee/Assets/SCRIPTS/script1.cs
+++ Repaso_yipee/Assets/SCRIPTS/script1.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class boxcollider: MonoBehaviour
 {
+    
+       
+   
     [SerializeField]
 
    private BoxCollider _collider ;
+
+    public class lista = new class[];
     void Start()
     {
-        
+        Lafuncion();
     }
 
 
     void Update()
     {
-        
+        Lafuncion();
+    }
+
+    public void Lafuncion()
+    {
+      
+
+
+
     }
 }
